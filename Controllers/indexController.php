1fed339d534ab38c11cd549b316b463e44183adb
DIAN <diandrapalaciosgutierrez@gmail.com>
2024-01-22 13:29:34 -0300

    cambios de ruta

--- Controllers/indexController.php
+++ Controllers/indexController.php
@@ -1,27 +1,24 @@
-<?php 
+<?php
 require_once("../Models/existetablaModel.php");
 $existe = new ExisteTabla();
-$tablas = ["Perfiles","Usuarios","Estados","CentrosMedicos","Examenes","Diagnosticos","Pacientes"];
+$tablas = ["Perfiles", "Usuarios", "Estados", "CentrosMedicos", "Examenes", "Diagnosticos", "Pacientes"];
 $validacionExistencia = true;
 
 
-foreach ($tablas as $tabla)
-{
+foreach ($tablas as $tabla) {
     $query = $existe->comprobarTabla($tabla);
-    if (!$query)
-    {
+    if (!$query) {
         $validacionExistencia = false;
     }
 }
 
 //Creacion de tablas si no existen.
-if (isset($_POST['crearTabla'])) 
-{
+if (isset($_POST['crearTabla'])) {
     $existe->crearTablas();
     $validacionExistencia = true;
+    echo '<div class="alert alert-success" role="alert">
+    Tablas creadas con Exito!!
+    </div>';
 }
 
-
-
-
-require_once ("../index.php");
+require_once("../index.php");
