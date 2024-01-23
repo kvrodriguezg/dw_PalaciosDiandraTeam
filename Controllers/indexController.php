a7ab399bba0deeff25df440e15e4804311e34286
kv.rodriguezgarcia@gmail.com <kv.rodriguezgarcia@gmail.com>
2024-01-22 21:09:59 -0300

    Se arregla error de crear tablas

--- Controllers/indexController.php
+++ Controllers/indexController.php
@@ -1,9 +1,8 @@
 <?php
-require_once("../Models/existetablaModel.php");
+include("Models/existetablaModel.php");
 $existe = new ExisteTabla();
 $tablas = ["Perfiles", "Usuarios", "Estados", "CentrosMedicos", "Examenes", "Diagnosticos", "Pacientes"];
 $validacionExistencia = true;
-
 
 foreach ($tablas as $tabla) {
     $query = $existe->comprobarTabla($tabla);
@@ -15,10 +14,7 @@ foreach ($tablas as $tabla) {
 //Creacion de tablas si no existen.
 if (isset($_POST['crearTabla'])) {
     $existe->crearTablas();
+    echo "<script>alert('Creacion de Tablas')</script>";
     $validacionExistencia = true;
-    header('Location: ../index.php');
 }
 
-
-require_once ("../index.php");
-require_once ("../css/style.css");
--- index.php
+++ index.php
@@ -27,13 +27,13 @@
                 </ul>
             </nav>
             <?php
-            $validacionExistencia = '';
+            require_once("Controllers/indexController.php");
             if (!$validacionExistencia) {
                 echo '
                 <nav class="nav">
                 <ul class="nav">
                     <div class="m-1">
-                        <form method="post" action="./Controllers/indexController.php">
+                        <form method="post" action="index.php">
                             <input type="hidden" name="crearTabla" value="crear">
                             <button class="btn w-100 m-1 btn-primary btn-sm ">Crear Tabla</button>
                         </form>
