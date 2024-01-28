320903f43008ffbdf392702ddf90458052049716
Dian <diandrapalaciosgutierrez@gmail.com>
2024-01-27 23:38:55 -0300

    ingreso examen y paciente

--- Controllers/ExamenController.php
+++ Controllers/ExamenController.php
@@ -1,20 +1,47 @@
 <?php
 include "../Models/creacionexamenModel.php";
+//include "../Models/validacionrutModel.php";
 
-$examen= new examenModel();
-
-
+$examen = new ExamenModel();  
+$centrosmedicos=$examen->obtenerCentrosmedicos();
+//$valrut = new rutModel();
 
 if (!empty($_POST["ingreso"])) {
-    if (!empty($_POST["nombre"]) && !empty($_POST["domicilio"]) && !empty($_POST["rut"])) {
-        $nombre=$_POST["nombre"];
-        $domicilio=$_POST["domicilio"];
-        $rut=$_POST["rut"];
-        $validacion = $examen->validarPaciente($rut);
-        if($validacion==false){
-            $insertar=$examen->insertarPaciente($nombre,$domicilio,$rut);
+    if (!empty($_POST["nombre"]) && !empty($_POST["domicilio"]) && !empty($_POST["rut"]) && !empty($_POST["nombreexamen"]) && !empty($_POST["fechamuestra"])){
+        $nombre = $_POST["nombre"];
+        $domicilio = $_POST["domicilio"];
+        
+        $rut = $_POST["rut"];
+        /*if($valrut->validarut($rut)=="MAL"){
+            echo '<script>alert("Algunos campos están vacíos");</script>';
+        }*/
+        $nombreexamen = $_POST["nombreexamen"];
+        $fechamuestra = $_POST["fechamuestra"];
+        $fecharecepcion = $_POST["fecharecepcion"];
+        $idcentrosoli = $_POST["idcentrosoli"];
+        
+        $existePaciente = $examen->validarPaciente($rut);
+
+        if ($existePaciente) {
+            $domicilioActual = $examen->obtenerDomicilioPaciente($rut);
+            if ($domicilioActual != $domicilio) {
+                $examen->actualizarDomicilioPaciente($rut, $domicilio);
+                echo '<script>alert("Domicilio actualizado correctamente");</script>';
+            }
+        } 
+        if ($existePaciente==false) {
+            $examen->insertarPaciente($nombre, $domicilio, $rut);
+            $examen->insertarExamen($nombreexamen, $rut, $idcentrosoli, $fechamuestra, $fecharecepcion);
+            echo '<script>alert("Paciente registrado correctamente");</script>';
+            header("Location: recepcion.php"); 
+        } else{
+            $examen->insertarExamen($nombreexamen, $rut, $idcentrosoli, $fechamuestra, $fecharecepcion);
+            echo '<script>alert("Paciente ya existe. Se ha registrado el examen.");</script>';
+            header("Location: recepcion.php"); 
         }
-    }else{
-        echo '<script>alert("Algunos campos estan vacios");</script>';
+    } else {
+        echo '<script>alert("Algunos campos están vacíos");</script>';
     }
 }
+
+?>
--- Models/creacionexamenModel.php
+++ Models/creacionexamenModel.php
@@ -32,5 +32,76 @@ class examenModel {
             return false;
         }
     }
+
+    public function insertarExamen($nombreexamen,$rut,$idcentrosoli,$fechamuestra,$fecharecepcion){
+        $nombreexamen = mysqli_real_escape_string($this->db, $nombreexamen);
+        $rut = mysqli_real_escape_string($this->db, $rut);
+        $idcentrosoli = mysqli_real_escape_string($this->db, $idcentrosoli);
+        $fechamuestra = mysqli_real_escape_string($this->db, $fechamuestra);
+        $fecharecepcion = mysqli_real_escape_string($this->db, $fecharecepcion);
+        $query = "INSERT INTO examenes (NombreExamen,RutPaciente,IDCentroSolicitante,FechaTomaMuestra,FechaRecepcion, IDEstado) VALUES ('$nombreexamen','$rut','$idcentrosoli','$fechamuestra',NOW(), (SELECT IDEstado FROM estados WHERE NombreEstado = 'Recepcionado'));";
+        /*if ($stmt = mysqli_prepare($this->db, $query)) {
+            mysqli_stmt_bind_param($stmt, "ssis", $nombreexamen,$rut,$idcentrosoli,$fechamuestra,$fecharecepcion);
+            if (mysqli_stmt_execute($stmt)) {
+                return true;
+            } else {
+                return false;
+            }
+            
+        }*/
+        $result = mysqli_query($this->db, $query);
+
+        if ($result) {
+            return $result;
+        } else {
+            return "Error: " . mysqli_error($this->db);
+        }
+    }
+
+    public function obtenerCentrosmedicos()
+    {
+        $query = "SELECT * FROM centrosmedicos";
+        $result = mysqli_query($this->db, $query);
+        
+        if ($result) {
+            return $result;
+        } else {
+            return "Error: " . mysqli_error($this->db);
+        }
+    }
+    public function obtenerDomicilioPaciente($rut) {
+        $query = "SELECT DomicilioPaciente FROM pacientes WHERE RutPaciente = '$rut'";
+        $result = mysqli_query($this->db, $query);
+
+        if ($result) {
+            $row = mysqli_fetch_row($result);
+            return $row[0]; 
+        } else {
+            return "Error: " . mysqli_error($this->db);
+        }
+    }
+
+    public function actualizarDomicilioPaciente($rut, $nuevoDomicilio) {
+        $nuevoDomicilio = mysqli_real_escape_string($this->db, $nuevoDomicilio);
+
+        $query = "UPDATE pacientes SET DomicilioPaciente = '$nuevoDomicilio' WHERE RutPaciente = '$rut'";
+        mysqli_query($this->db, $query);
+
+        if (mysqli_error($this->db)) {
+            echo "Error al actualizar domicilio: " . mysqli_error($this->db);
+        }
+    }
+    /*public function insertarFechaRecepcion($fecharecepcion) {
+        $query = "INSERT INTO examenes (FechaRecepcion) VALUES (NOW())";
+        $fecharecepcion = mysqli_real_escape_string($this->db, $fecharecepcion);
+        mysqli_query($this->db, $query);
+    
+        if (mysqli_error($this->db)) {
+            echo "Error al insertar la fecha: " . mysqli_error($this->db);
+        } else {
+            echo "Fecha actual insertada correctamente";
+        }
+    }
+    */
 }
 ?>
