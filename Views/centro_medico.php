c08a0bedd2d0a8d23eb20b0115396ede56ebd63c
Alexis Tobar <drokuas@gmail.com>
2024-01-29 14:53:34 -0300

    commit

--- Views/centro_medico.php
+++ Views/centro_medico.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="icon" type="image/svg+xml" href="~/favicon.ico" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <title>Document</title>
+    <title>Centro Medico</title>
 </head>
 
 <header class="navbar navbar-light fixed-top" style="background-color: #9CD0FE;">
